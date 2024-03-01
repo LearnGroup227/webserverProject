bcebf388534b5fdb87fc987bf17626f0c0782155
Rinny <artpr.zxc@gmail.com>
2024-03-01 16:19:39 +0500

    new task

--- webserver/client.py
+++ webserver/client.py
@@ -1,12 +1,23 @@
 import socket
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 client_sock.connect(('192.168.0.105', 53210))
 while True:
     request = input()
     client_sock.sendall(bytes(request, 'utf-8'))
     if '!' in request:
         break
-data = client_sock.recv(1024)
-client_sock.close()
-print(repr(data))
+
+def get_server_data() -> str:
+    data = bytearray()
+    while True:
+        respond = client_sock.recv(1024)
+        if not respond:
+            break
+        data += respond
+    return data.decode('utf-8')
+
+print(get_server_data())
+
+# Вопрос: как понять, что сервер нам что-то отправляет (или не отправляет) без использования sock.recv()? 
+#    или: как понять, что сервер больше не ждёт данных? (это менее реализуемо)
